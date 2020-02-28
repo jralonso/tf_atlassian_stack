b044b15d6d0fdf8d333593cf41f445e615557c1d
juanra <JuanRamon.AlonsoGarcia@zooplus.com>
2020-02-28 10:32:25 +0100

    Feat: no terraform state will be pushed to repository

---
+++ dev/global/data-storage/variables.tf
@@ -0,0 +1,13 @@
+# #####################################
+# Account defaults
+# #####################################
+
+# Define Region for the infraestructure
+variable "region" {
+  default = "eu-central-1"
+}
+
+# Profile to execute this plan
+variable "profile" {
+  default = "575877736355_OperatorAccess"
+}
